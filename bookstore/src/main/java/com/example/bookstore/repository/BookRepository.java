8962283d51ddadd58d28153d32e1f5ecfd1de098
jihohyeseong <152616772+jihohyeseong@users.noreply.github.com>
2024-05-31 09:56:51 +0900

    Delete bookstore directory

--- bookstore/src/main/java/com/example/bookstore/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstore.repository;
-
-import com.example.bookstore.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
